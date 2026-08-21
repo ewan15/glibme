865778a4c143b3bdbfd46892a9957af8e6991eeb
Ewan Bains <ewanbains@Ewans-MBP.powerhub>
2026-08-21 22:33:50 +0100

    Implement puts with write syscall

--- src/stdio.cpp
+++ src/stdio.cpp
@@ -1,10 +1,40 @@
 #include "glibme.hpp"
+#include <cstddef>
+#include <unistd.h>
 
 namespace glibme {
 
+static bool write_all(int fd, const char *s, std::size_t size)
+{
+  while (size > 0) {
+    ssize_t amount_written = write(fd, s, size);
+    if (amount_written <= 0) {
+      return false;
+    }
+
+    const auto amount_written_size_t = static_cast<std::size_t>(amount_written);
+    s += amount_written_size_t;
+    size -= amount_written_size_t;
+  }
+  return true;
+}
+
 int puts(const char *s)
 {
-  (void)s;
+  const char *p = s;
+  while (*p != '\0') {
+    ++p;
+  }
+  const auto size = static_cast<std::size_t>(p - s);
+
+  if (!write_all(STDOUT_FILENO, s, size)) {
+    return -1;
+  }
+
+  if (!write_all(STDOUT_FILENO, "\n", 1)) {
+    return -1;
+  }
+
   return 0;
 }
 
